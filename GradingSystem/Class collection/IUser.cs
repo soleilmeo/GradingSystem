a5e5619ea013e42542f802087d664b09dc667481
Elysia <96197966+soleilmeo@users.noreply.github.com>
2024-05-18 05:20:03 +0700

    Add interface IUser

---
+++ GradingSystem/Class collection/IUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem.Class_collection
+{
+    public interface IUser
+    {
+        int Create(
+            string firstName,
+            string lastName,
+            string username,
+            string password,
+            string email,
+            DateTime dateOfBirth,
+            string phoneNumber);
+        void Update(
+            string id,
+            string? firstName = null,
+            string? lastName = null,
+            string? username = null,
+            string? password = null,
+            string? email = null,
+            DateTime? dateOfBirth = null,
+            string? phoneNumber = null);
+        void Delete(string id);
+    }
+}
